65dedc379e4f8eb671dadad87a07f5af54a55305
Hack Reactor Student <student@hackreactor.com>
2020-01-03 16:42:28 -0700

    Adds Get function to server and client

--- client/main.js
+++ client/main.js
@@ -1,3 +1,5 @@
+//import { brotliDecompressSync } from "zlib";
+
 $(document).ready(function() {
 
 // get a quote from the server when the page loads and add it to the dom
@@ -8,17 +10,41 @@ $(document).ready(function() {
     e.preventDefault();
     let quote = $('input').val();
     addQuote(quote);
+    
   });
 
   function getQuote(){
 
-    //YOUR CODE HERE, Add a GET request
+    //YOUR CODE HERE, Add a GET request 
+    // var getObj = $.ajax({
+    //   type: "GET",
+    //   data: {1: this.responseText},
+    //   url: 'http://localhost:3000',
+    //   success: console.log("get request sent"),
+    // })
+    // // console.log($.ajax({
+    // //   type: "GET",
+    // //   url: 'http://localhost:3000',
+    // //   success: console.log("get request sent"),
+    // // }))
+    // console.log()
+    // $('body').append()
 
+    $.get('http://localhost:3000', function(data){
+      $('#quote').text(data);
+  })
   }
 
   function addQuote(quote){
     
     //YOUR CODE HERE, Add a POST request
+    $.ajax({
+      type: "POST",
+      data: quote,
+      url: 'http://localhost:3000',
+      success: console.log("post request sent"),
+      // error: console.log("get request failed")
+    })
 
   }
 });
--- server/index.js
+++ server/index.js
@@ -12,11 +12,11 @@ const port = 3000;
 
 // TODO: Fill with strings of your favorite quotes :)
 const quotes = [
-  'one',
-  'two',
-  'three',
-  'four',
-  'five'
+  'Fake It Til You Make It',
+  'Do Or Do Not, There Is No Try - Yoda',
+  'Even The Smallest Person Can Change The Course Of the Future- Galadriel',
+  'You Miss 100% Of The Shots You Dont Take - Wayne Gretzky - Michael Scott',
+  'I Am Ready To Face Any Challenge That Might Be Foolish Enough To Face Me - Dwight Schrute'
 ];
 
 //Utility Function to return a random integer
@@ -27,7 +27,7 @@ function getRandomInt(min, max) {
 }
 
 const handleRequest = function(req, res) {
-  console.log(`Endpoint: ${req.url} Method: ${req.method}`);
+  console.log(`Endpoint: ${req.url} Method: ${req.method} Res: ${res}`);
 
   // redirect users to /quote if they try to hit the homepage. This should already work, no changes needed
   if (req.url == '/') {
@@ -37,13 +37,21 @@ const handleRequest = function(req, res) {
   }
 
   // TODO: GET ONE
-  if ((req.url == '/quote/' || req.url == '/quote') && req.method == "FILL ME IN") {
+  if ((req.url == '/quote/' || req.url == '/quote') && req.method == "GET") {
     //YOUR CODE HERE
+    console.log("get method received to server")
+
+    res.writeHead(200, {...headers})
+    res.end(quotes[getRandomInt(0,quotes.length-1)]);
+
+    // req.response(quotes)
 
   }
   // TODO: POST/CREATE
-  else if ((req.url == 'FILL ME IN' || req.url == 'FILL ME IN') && req.method == "FILL ME IN") {
+  
+  else if ((req.url == 'quote' || req.url == '/quote') && req.method == "POST") {
     //YOUR CODE HERE
+    console.log("post method correct from server")
   }
 
 //CATCH ALL ROUTE
